ffb078d1cd3bd491a42d15f968f169706246c942
Tejas Tirthapura <tirthapuratejas@gmail.com>
2024-12-22 15:30:43 -0800

    added getHumanChoice

--- script.js
+++ script.js
@@ -2,12 +2,25 @@ function getComputerChoice() {
     let x = Math.floor(Math.random() * 3);
 
     if (x === 0) {
-        return "Rock";
+        return "rock";
     }
     if (x == 1) {
-        return "Paper";
+        return "paper";
     }
     if (x == 2) {
-        return "Scissors";
+        return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    let choice = prompt("Rock, Paper, or Scissors? ");
+    if (choice.toLowerCase() === "rock") {
+        return "rock";
+    }
+    if (choice.toLowerCase() === "paper") {
+        return "paper";
+    }
+    if (choice.toLowerCase() === "scissors") {
+        return "scissors";
     }
 }
