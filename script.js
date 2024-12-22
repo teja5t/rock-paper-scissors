ba92ffca52d34e322c61867ad819b0a7b3c88981
Tejas Tirthapura <tirthapuratejas@gmail.com>
2024-12-22 15:25:49 -0800

    added getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let x = Math.floor(Math.random() * 3);
+
+    if (x === 0) {
+        return "Rock";
+    }
+    if (x == 1) {
+        return "Paper";
+    }
+    if (x == 2) {
+        return "Scissors";
+    }
+}
