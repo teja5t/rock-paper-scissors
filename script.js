4ccbb244a79807e23bcedeb2c5625d4d541da8ed
Tejas Tirthapura <tirthapuratejas@gmail.com>
2024-12-22 15:46:29 -0800

    made playRound

--- script.js
+++ script.js
@@ -23,4 +23,41 @@ function getHumanChoice() {
     if (choice.toLowerCase() === "scissors") {
         return "scissors";
     }
+
+    return getHumanChoice();
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "paper") {
+        computerScore++;
+        console.log("You lose! Paper beats Rock.");
+    }
+    else if (humanChoice === "rock" && computerChoice === "scissors") {
+        humanScore++;
+        console.log("You win! Rock beats Scissors.");
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+        humanScore++;
+        console.log("You win! Paper beats Rock.");
+    }
+    else if (humanChoice === "paper" && computerChoice === "scissors") {
+        computerScore++;
+        console.log("You lose! Scissors beats Paper.");
+    }
+    else if (humanChoice === "scissors" && computerChoice === "rock") {
+        computerScore++;
+        console.log("You lose! Rock beats Scissors.");
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
+        console.log("You win! Scissors beats Paper.");
+    }
+    else {
+        console.log(`Tie! You both selected ${computerChoice[0].toUpperCase() + computerChoice.substring(1)}.`);
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+}
+
